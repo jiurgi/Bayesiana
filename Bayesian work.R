7e8123a385274a74dcc551b9c69dd7c662ea348c
Daniel Puentes <101430867+dapuentes@users.noreply.github.com>
2023-05-18 22:25:12 -0500

    Update Bayesian work.R
    Ay, no se xd

--- Bayesian work.R
+++ Bayesian work.R
@@ -10,4 +10,56 @@ as.factor(datos$RVE5)
 barplot(datos$RVE5, datos$CS2)
 
 
+'''library(rstan)
+
+
+
+modelo <- "
+data {
+  int<lower=0> N;                // Número de observaciones
+  vector[N] edad;                // Variable edad
+  int<lower=1, upper=2> sexo[N]; // Variable sexo codificada como 1 = Masculino, 2 = Femenino
+  int<lower=1, upper=3> estrato[N]; // Variable estrato socioeconómico codificada como 1 = Bajo, 2 = Medio, 3 = Alto
+  int<lower=0, upper=1> y[N];     // Variable dependiente codificada como 0 = No, 1 = Sí
+}
+
+parameters {
+  real alpha;                    // Intercepto
+  real beta_edad;                // Coeficiente de edad
+  real beta_sexo;                // Coeficiente de sexo
+  real beta_estrato;             // Coeficiente de estrato socioeconómico
+}
+
+model {
+  vector[N] p;                   // Probabilidades estimadas
+
+  // Prior
+  alpha ~ normal(0, 2.5);
+  beta_edad ~ normal(0, 2.5);
+  beta_sexo ~ normal(0, 2.5);
+  beta_estrato ~ normal(0, 2.5);
+
+  // Likelihood
+  for (i in 1:N) {
+    p[i] = inv_logit(alpha + beta_edad * edad[i] + beta_sexo * sexo[i] + beta_estrato * estrato[i]);
+    y[i] ~ bernoulli(p[i]);
+  }
+}
+"
+
+dato <- list(
+  N = dim(datos)[1],
+  edad = datos$CS2,
+  sexo = datos$CS1,
+  estrato = datos$CCS2,
+  y = datos$RVE5
+)
+
+ajuste <- stan(model_code = modelo,
+               data = dato,
+               iter = 2000,
+               chains = 4)
+
+print(ajuste)'''
+
 
